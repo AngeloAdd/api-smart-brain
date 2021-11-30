645fccc8c6048e0a144b02d791a3a9104c3d2424
Angelo <angelo.adduci1991@gmail.com>
2021-11-30 08:38:25 +0100

    Update server to use controller

--- server.js
+++ server.js
@@ -1,11 +1,14 @@
 require('dotenv').config({path: './.env'})
 const config = require('./config/config')
+const userController = require('./controllers/userController')
+const imageController = require('./controllers/imageController')
 
 const express = require('express')
 const cors = require('cors')
 const app = express()
 const bcrypt = require('bcrypt-nodejs')
 const knex = require('knex')
+const Clarifai = require('clarifai')
 
 const defaultUserInfo = ['id', 'username', 'email', 'rank', 'created_at', 'updated_at']
 
@@ -22,83 +25,14 @@ app.get('/', (req, res) => {
     .catch(e => console.log(e))
 })
 
-app.post('/signin', (req, res) => {
-    const { email, password } = req.body
+app.post('/user/login', userController.login(db, bcrypt, defaultUserInfo))
 
-    return db('login')
-        .select('email', 'password')
-        .where({email})
-        .then(user => {
-            if(bcrypt.compareSync(password, user[0].password)){
-                return db('users')
-                .select(defaultUserInfo)
-                .where({
-                    email: user[0].email,
-                })
-                .then(data => res.json(data[0]))
-                // eslint-disable-next-line no-unused-vars
-                .catch(e => res.status(400).json('Unable to get user!'))
-            }
-        })
-        // eslint-disable-next-line no-unused-vars
-        .catch(e => res.status(400).json('User info are wrong'))
-})
+app.post('/user/create', userController.create(db, bcrypt, defaultUserInfo))
 
-app.post('/register', (req, res) => {
-    const { username, email, password} = req.body
-    const now = new Date()
-    const hash = bcrypt.hashSync(password)
+app.get('/user/:id', userController.show(db, defaultUserInfo))
 
-    return db.transaction(trx => {
-        return Promise.all([
-            trx('users').insert({
-                username,
-                email,
-                rank: 0,
-                created_at: now,
-                updated_at: now,
-            })
-            .returning(defaultUserInfo),
-            trx('login').insert({
-                email,
-                password: hash,
-            }),
-        ])
-        .then(user => res.json(user[0]))
-        .then(trx.commit)
-        .catch(trx.rollback)
-    })
-    // eslint-disable-next-line no-unused-vars
-    .catch(e => res.status(400).json('Registration failed. A user with this email already exists.'))
-})
+app.post('/image/show', imageController.show(Clarifai, config))
 
-app.get('/profile/:id', (req, res) => {
-    const id = parseInt(req.params.id)
-    
-    return db('users')
-        .select(defaultUserInfo)
-        .where({id})
-        .then(user => {
-            if(user.length){
-                return res.json(user[0])
-            }
-
-            return res.status(404).json('User not found!')
-        })
-        // eslint-disable-next-line no-unused-vars
-        .catch(e => res.status(400).json('Error getting user info. Try again later!'))
-})
-
-app.put('/image', (req, res) => {
-    const id = parseInt(req.body.id)
-    
-    return db('users')
-        .where({id})
-        .increment('rank', 1)
-        .returning('rank')
-        .then(rank => res.json({rank:rank[0]}))
-        // eslint-disable-next-line no-unused-vars
-        .catch(e => res.status(404).json('Unable to get the rank. User not found.'))
-})
+app.put('/user/update/rank', userController.updateRank(db))
 
 app.listen(config('app').port)
