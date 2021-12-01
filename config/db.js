6d6f97e10e4d27f2cabda57b270e48541289989e
Angelo <angelo.adduci1991@gmail.com>
2021-12-01 13:58:06 +0100

    Add Host env

--- config/db.js
+++ config/db.js
@@ -3,7 +3,7 @@ const env = require('../utilities/env')
 module.exports = {
     client: env('DATABASE_CLIENT', 'postgres'),
     connection: {
-        host : env('DATABASE_URL', 'localhost'),
+        host : env('DATABASE_HOST', 'localhost'),
         port : env('DATABASE_PORT', 5432),
         user : env('DATABASE_USER', 'Angelo'),
         password : env('DATABASE_PASSWORD', ''),
