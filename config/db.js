c392304cdabb60fe74cff42c4c9bf8e32c6e94da
Angelo <angelo.adduci1991@gmail.com>
2021-12-01 15:07:19 +0100

    Fix string connection

--- config/db.js
+++ config/db.js
@@ -4,8 +4,8 @@ const db = {
     production : {
         client: env('DATABASE_CLIENT', 'postgres'),
         connection : {
-            connectionString : env('DATABASE_URL', 'localhost') + '?sslmode=require',
-            ssl : {
+            connectionString : env('DATABASE_URL', 'localhost'),
+            ssl : { 
                 rejectUnauthorized: false,
             },
         },
